5f0b7f0815c815399fc2bb6690451f00b5e58ef5
Connor Johnson <connor.labaume.johnson@gmail.com>
2014-07-18 22:24:24 -0500

    Create kriging_example.py

---
+++ kriging_example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import utilities as u
+import krige as k
+
+# grab the data from cluster.dat
+d = u.readGeoEAS("cluster.dat")
+
+# use only the first three columns
+d = d[:,:3]
+
+# hs is an array of distances to use in semivariogram modeling
+hs = np.linspace( 0, 50, 10 )
+
+# bw is the bandwidth, the plus/minus distance around
+# the distances listed in hs to use in semivariogram modeling
+bw = 5
+
+# for this example, z should be 3.9225013671953293
+z = k.krige( d, k.spherical, hs, bw, [25.,25.], 4 )
