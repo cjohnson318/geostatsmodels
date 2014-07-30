b70d780891a7eb84975e7de2f20f6dc49c8e81fb
Connor Johnson <connor.labaume.johnson@gmail.com>
2014-07-30 11:18:51 -0500

    Update kriging_example.py

--- kriging_example.py
+++ kriging_example.py
@@ -11,12 +11,10 @@ d = u.readGeoEAS("cluster.dat")
 # use only the first three columns
 d = d[:,:3]
 
-# hs is an array of distances to use in semivariogram modeling
-hs = np.linspace( 0, 50, 10 )
+# lags is an array of distances to use in semivariogram modeling
+lags = np.linspace( 10, 50, 10 )
 
-# bw is the bandwidth, the plus/minus distance around
-# the distances listed in hs to use in semivariogram modeling
-bw = 5
+# tolerance is a plus/minus distance around the lags
+tol = 5
 
-# for this example, z should be 3.9225013671953293
-z = k.krige( d, k.spherical, hs, bw, [25.,25.], 4 )
+z = k.krige( d, k.spherical, lags, tol, [25.,25.] )
