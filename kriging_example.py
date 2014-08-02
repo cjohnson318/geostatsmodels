84fbda7f16ad21e37d44ed016b80d4d082527dfe
Connor Johnson <connor.labaume.johnson@gmail.com>
2014-08-02 16:56:37 -0500

    Update kriging_example.py

--- kriging_example.py
+++ kriging_example.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
 import numpy as np
-
 import geostatsmodels.utilities as u
+import geostatsmodels.models as m
 import geostatsmodels.kriging as k
 
-# grab the data from cluster.dat
-d = u.readGeoEAS("cluster.dat")
+# load data from the GeoEAS data format
+data = u.readGeoEAS("data/cluster.dat")
 
-# use only the first three columns
-d = d[:,:3]
-
-# lags is an array of distances to use in semivariogram modeling
-lags = np.linspace( 10, 50, 10 )
-
-# tolerance is a plus/minus distance around the lags
+# define a +/- tolerance for the lags
 tol = 5
 
-z = k.krige( d, k.spherical, lags, tol, [25.,25.] )
+# create a NumPy array of lags
+lags = np.arange( 10, 50, tol*2 )
+
+# create a function to model the variance
+covfct = m.covmodel( data, m.gaussian, lags, tol )
+
+# specify a point to estimate
+z = [ 25.0, 25.0 ]
+
+# pass that function to the kriging unction
+kv = nk.krige( data, covfct, lags, tol, z )
+
+print kv
