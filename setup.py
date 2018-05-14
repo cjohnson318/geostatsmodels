ab86642843f69d0addf22ef83db5b5ba6ced890a
Connor Johnson <connor.labaume.johnson@gmail.com>
2018-05-14 11:53:08 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='geostatsmodels',
-      version='0.1',
+      version='0.1.1',
       description='Geostatistics models',
       author='Connor Johnson',
       author_email='connor.labaume.johnson@gmail.com',
